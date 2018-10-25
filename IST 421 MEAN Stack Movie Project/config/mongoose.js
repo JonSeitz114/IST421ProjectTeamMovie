c230442f89f0462823a154b0cf718f3e83386ec9
JonSeitz114 <42967026+JonSeitz114@users.noreply.github.com>
2018-10-25 13:57:07 -0400

    Files Up To Authentication Lab
    Will add in Angular.js files after next lab.

---
+++ IST 421 MEAN Stack Movie Project/config/mongoose.js
@@ -0,0 +1,30 @@
+var config = require('./config'),
+mongoose = require('mongoose')
+console.log(`Mongoose: ${mongoose.version}`)
+MongoClient = require('mongodb').MongoClient;
+
+
+module.exports = function() {
+var db = mongoose.connect(config.db, {useNewUrlParser: true}).then(
+ (res) => {
+   console.log("Connected to Database Successfully.")
+  }
+).catch(() => {
+  console.log("Connection to database failed.");
+});
+
+MongoClient.connect(config.db,{ useNewUrlParser: true },function(err,db){
+  if(err){
+      console.log(err);
+  }
+  else {
+      console.log('connected to '+ config.db);
+      db.close();
+  }
+});
+
+
+require('../app/models/user.server.model');
+
+return db;
+};
